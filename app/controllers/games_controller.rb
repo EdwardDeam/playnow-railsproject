a570e3828b70fd56f16ec1a64e7c452aa0e9ea82
Edward Deam <edwardcrdeam@gmail.com>
2019-05-01 11:49:53 +1000

    Added show and html display for individual games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,8 @@
 class GamesController < ApplicationController
+  def show
+    # @book = Book.find(params[:id])
+    # @review = Review.new
+
+    @game = Game.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/games/:id', to:'games#show'
+  devise_for :users
+  # resources :games
+
   root 'welcome#index'
 end
