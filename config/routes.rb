1cc6867322b757d77a6a5e6bf006b0e899bff3a3
Edward Deam <edwardcrdeam@gmail.com>
2019-05-01 12:15:31 +1000

    Added cancan Ability definitons, updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get '/games/:id', to:'games#show'
   devise_for :users
-  # resources :games
+  resources :games
 
   root 'welcome#index'
 end
