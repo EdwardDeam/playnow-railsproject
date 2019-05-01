64bfe968054950b44ff15b4d8fc7c9bb10935d4a
Cooper Viktor <im.cooperviktor@gmail.com>
2019-05-01 10:03:17 +1000

    morning commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one :publisher, dependent: :destroy
   has_many :orders
+  # validates :age_restriction
+
+  # def age_restriction
+  #   if (birth_date.to_date + 13.years) < Date.strptime
+  #     errors.add :birth_date, 'Sorry but you need to be over 13 to agree to the Terms & Conditions'
+  #   end
+  # end
 end
