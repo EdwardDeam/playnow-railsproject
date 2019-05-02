413febd4000641ba3ed67a460608120bd4cd343d
Edward Deam <edwardcrdeam@gmail.com>
2019-05-02 12:59:08 +1000

    Added method to Game model and contoller to update and sanitize price

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @game = Game.new(
+      title: 'Test Game',
+      genre: 'Strategy',
+      price: 1567,
+      description: 'This is the description')
+  end
+
+  def test_price
+    assert_equal 1567, @game.price
+  end
+
+  def test_price_to_decimal
+    assert_equal 15.67, @game.price_to_decimal
+  end
+
+  def test_decimal_to_price
+    assert_equal 5793, @game.decimal_to_price(5793)
+  end
 end
