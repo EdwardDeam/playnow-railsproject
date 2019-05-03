044893763a3e1c9e8bae37043677036a656c50f9
Edward Deam <edwardcrdeam@gmail.com>
2019-05-03 14:37:37 +1000

    Dashboard builds a custom list of owned games

--- app/controllers/dashboard_controller/game_list.rb
+++ app/controllers/dashboard_controller/game_list.rb
@@ -3,10 +3,25 @@ class DashboardController
   # Used for storing and sorting a list of user specific Game objects for display
   # on the dashboard page
   class GameList
-    attr_accessor :user
+    attr_accessor :all
     def initialize(current_user)
-      Debug.console('GameList Current User', current_user.username)
-      @user = current_user
+      @orders = current_user.orders.includes(:game, :publisher)
+      @all = build_all
     end
+
+    private
+
+    def build_all
+      games = []
+      @orders.each do |o|
+        games << {
+          title: o.game.title,
+          publisher: o.publisher.user.username,
+          key: o.game_key
+        }
+      end
+      return games
+    end
+
   end
 end
