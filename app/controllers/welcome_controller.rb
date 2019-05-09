377f34359d33918a544fa9481b66cc8bb82e4cc3
Edward Deam <edwardcrdeam@gmail.com>
2019-05-09 14:17:34 +1000

    Fixing user avatar

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,8 @@ class WelcomeController < ApplicationController
     # A list of all active games.
     @list = GameList.new
     user = User.find(current_user.id)
+    Debug(user)
+    Debug(user.avatar)
     @user_avatar = user.avatar
   end
 end
