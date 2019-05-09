4f7a77d25077091cfc588a421158ebe76a611720
Edward Deam <edwardcrdeam@gmail.com>
2019-05-09 12:05:15 +1000

    Added image tag to user image

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,7 @@ class WelcomeController < ApplicationController
   def index
     # A list of all active games.
     @list = GameList.new
+    user = User.find(current_user.id)
+    @user_avatar = user.avatar
   end
 end
